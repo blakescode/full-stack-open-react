bfdf38a3c8cfde6c20fa7fe00634bae812ed9d08
blakescode <rydyell8@gmail.com>
2021-10-01 20:10:08 -0400

    part-one D, complex state + event handlers

--- part-one/src/App.js
+++ part-one/src/App.js
@@ -1,35 +1,62 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-  const setToZero = () => setCounter(0)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  const setToValue = (newValue) => () => {
+    setLeft(newValue)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
+      {left}
+      <button onClick={setToValue(1000)}>Thousand</button>
+      <button onClick={setToValue(0)}>Zero</button>
       <Button 
-        onClick={increaseByOne}
-        text={'plus'}
+        handleClick={handleLeftClick}
+        text='Left'
       />
-      <Button 
-        onClick={setToZero}
-        text={'zero'}
+      <Button
+        handleClick={handleRightClick}
+        text='Right'
       />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <button onClick={onClick}>
-      {text}
-    </button>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 export default App
