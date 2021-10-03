59cca105401e9494ce513b6244148cc4d6fdfca8
blakescode <rydyell8@gmail.com>
2021-10-03 17:02:20 -0400

    2.14, add weather data from weatherstack

--- part-two/country-data/src/components/CountryDetails.js
+++ part-two/country-data/src/components/CountryDetails.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const CountryDetails = ({ country }) => {
+  const weather_api_key = process.env.REACT_APP_WEATHER_KEY
+  const weather_url = 'http://api.weatherstack.com/current?access_key=' + weather_api_key + '&query=' + country.capital
+  const [ currentWeather, setCurrentWeather ] = useState()
+
+  useEffect(() => {
+    axios
+      .get(weather_url)
+      .then(response => {
+        setCurrentWeather(response.data.current)
+      })
+  }, [weather_url])
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -15,6 +28,14 @@ const CountryDetails = ({ country }) => {
         })}
       </ul>
       <img src={country.flags.png} alt={country.name}/>
+      {currentWeather &&
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <p>temperature: {currentWeather.temperature}</p>
+          <img src={currentWeather.weather_icons[0]} alt='Weather icon'/>
+          <p>wind: {currentWeather.wind_speed} mph direction {currentWeather.dir}</p>
+        </div>
+      }
     </div>
   )
 }
